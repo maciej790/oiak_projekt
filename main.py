273f6235a5401c67a59a5d6e1e3c9d8f6070f3a8
maciej790 <60989274+maciej790@users.noreply.github.com>
2024-05-23 19:49:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,69 +1,130 @@
+#libki
 import struct
+import timeit
+import random
+import csv
 
 class Posit:
-    def __init__(self, n=16, es=2):
+    def __init__(self, n=32, es=2):
         self.n = n
         self.es = es
 
-    def posit_to_float(self, x):
-        # Krok 1: Pobranie znaku
-        sign = (x >> (self.n - 1)) & 1
+    #konwersja na z posit na float
+    def posit_na_float(self, posit_liczba):
 
-        # Krok 2: Pobranie wartości (bez znaku)
-        val = x & ((1 << (self.n - 1)) - 1)
+        znak = int(posit_liczba[0]) #sam znak
+        bez_znaku = int(posit_liczba[1:], 2) #liczba obcieta o znak
 
-        # Krok 3: Sprawdzenie czy wartość jest równa 0
-        if val == 0:
-            # Krok 4: Jeśli znak jest 0, ustaw y na 0, w przeciwnym razie na NaN
-            if sign == 0:
-                return 0.0
-            else:
-                return float('nan')
+        if bez_znaku == 0:
+            return -0.0 if znak else 0.0 #jesli same zera po znaku => 0, jesli znak = 1 => liczba ujemna, w przeciwnym wypadku => dodatnia
         else:
-            # Krok 9: Sprawdzenie znaku
-            if sign == 0:
-                abs_val = val
-            else:
-                # Krok 12: Obliczenie wartości bezwzględnej (2's complement)
-                abs_val = ((1 << (self.n - 1)) - 1) & ~val + 1
+            if znak != 0: #ujemna
+                # dopelnienie do 2
+                bez_znaku = ((1 << (self.n - 1)) - 1) & ~bez_znaku + 1  #przesuwanie bitow liczby i negacja NOT
 
-            # Krok 13: Ekstrakcja pól regime, exp, frac
-            regime, exp, frac = self.extract_fields(abs_val)
+            regime, wykladnik, mantysa = self.pobierz_pola_posit(bez_znaku) #wyluskanie pol posit
+            bias_mantysa = (regime << self.es) + wykladnik + (2**(8 - 1) - 1)  # mozna by odrazu 127 ale dla czytelnosci arytmetyki ieee :)
 
-            # Krok 14: Bias dla wykładnika IEEE float
-            bias = (2 ** (8 - 1)) - 1
-
-            # Krok 15: Obliczenie biased_exp
-            biased_exp = (regime << self.es) + exp + bias
-
-            # Krok 16: Skonstruowanie liczby IEEE float
-            ieee_float = self.construct_ieee_float(sign, biased_exp, frac)
-
-            # Krok 17: Dodanie zer na końcu, jeśli to konieczne (już uwzględnione w konstrukcji liczby IEEE float)
+            # na podstawie wyluskanych danych tworzymy reprezentacje floata
+            ieee_float = self.stworz_ieee_float(znak, bias_mantysa, mantysa)
             return ieee_float
 
-    def extract_fields(self, val):
+    def float_na_posit(self, float_bez_znaku):
+        # jesli liczba jest ujemna ustaw znak na 1 w przeciwnym na 0
+        znak = 0 if float_bez_znaku >= 0 else 1
+        float_bez_znaku = abs(float_bez_znaku)
+        #konwertowanie floata na binarke (4 bajty) - funkaja wbudowana w pythona
+        ieee_int = struct.unpack('!I', struct.pack('!f', float_bez_znaku))[0]
+
+        # wyodrebnianie pol ieee
+        znak_bit = (ieee_int >> 31) & 1 # bit znaku - przesuwamy o 31 w prawo i maska 1
+        wykladnik_float = (ieee_int >> 23) & 0xFF #wykladnik - przesuwamy o 23 w prawo i maska 0xff
+        mantysa_float = ieee_int & 0x7FFFFF #mantysa - to co zostalo i maska 
+
+        #liczba zdenormalizowana wtedy regime i exp na 0
+        if wykladnik_float == 0:
+            regime = 0
+            wykladnik = 0
+        #znormalizowana
+        else:
+            regime = (wykladnik_float - (2**(8 - 1) - 1)) >> self.es #odejmij przesuniecie bias i przesun w prawo o es
+            wykladnik = (wykladnik_float - (2**(8 - 1) - 1)) & ((1 << self.es) - 1) # przesun o tyle samo i maska
+
+        # jesli przesuniecie jest < 0 to mantysa jest wieksza od 23bit
+        przesuniecie_bitowe = (23 - (self.n - 2 - self.es)) #roznica bitow mantysy ieee a posit
+        if przesuniecie_bitowe < 0:
+            przesuniecie_bitowe = 0
+
+        #laczenie pol wykladnika i mantysy z przesunieciem
+        bez_znaku = (regime << (self.n - 2 - self.es)) | (wykladnik << (self.n - 2 - self.es - self.es)) | (mantysa_float >> przesuniecie_bitowe)
+        # laczymy z bitem znaku i mamy liczbe
+        posit_liczba = znak << (self.n - 1) | bez_znaku
+
+        return posit_liczba
+    
+    # pobieranie pol z posit
+    def pobierz_pola_posit(self, bez_znaku):
         k = 0
-        while val & (1 << (self.n - 2 - k)):
+        # dopoki napotkany bit jest 1
+        while bez_znaku & (1 << (self.n - 2 - k)) and k < self.n - 2: #czy bit na (self.n - 2 - k) jest 1 lub 0
             k += 1
 
         regime = k - 1
-        exp = (val >> (self.n - 2 - k - self.es)) & ((1 << self.es) - 1)
-        frac = (val & ((1 << (self.n - 2 - k - self.es)) - 1)) << (23 - (self.n - 2 - k - self.es))  # 23 is the number of bits in mantissa in IEEE 754
+        #przesuwaj bity bez_znaku w prawo o liczbe bitow równa (self.n - 2 - k - self.es) 
+        # aby uzyskac bity wykladnika na najmniej znaczących pozycjach
+        wykladnik = (bez_znaku >> (self.n - 2 - k - self.es)) & ((1 << self.es) - 1)
 
-        return regime, exp, frac
+        #liczba bitow potrzebnych na przesunieta mantyse ieee (23bity)
+        przesuniecie_bitowe = (23 - (self.n - 2 - k - self.es))
+        # jesli przesuniecie < 0 to 0 zeby nie przesuwac w lewo
+        if przesuniecie_bitowe < 0:
+            przesuniecie_bitowe = 0
 
-    def construct_ieee_float(self, sign, biased_exp, frac):
-        ieee_int = (sign << 31) | (biased_exp << 23) | (frac & ((1 << 23) - 1))
+        #wyodrebnij mantyse
+        mantysa = (bez_znaku & ((1 << (self.n - 2 - k - self.es)) - 1)) << przesuniecie_bitowe  # 23 liczba bitow mantysy ieee
+
+        return regime, wykladnik, mantysa
+
+    #tworzenie reprezenracji floata z posita
+    def stworz_ieee_float(self, znak, bias_mantysa, mantysa):
+        #przesun znak na najbrarziej znaczece miejsce (31) potem wykladnik od 23 pozycji a reszta mantysa
+        ieee_int = (znak << 31) | (bias_mantysa << 23) | (mantysa & ((1 << 23) - 1))
+        #skonweruj na binarke big endian float
         ieee_float = struct.unpack('!f', struct.pack('!I', ieee_int))[0]
         return ieee_float
 
+    def oblicz_blad_wzgledny(self, oryginalna_bez_znaku, skonwertowana_bez_znaku):
+        if oryginalna_bez_znaku == 0:
+            return abs(oryginalna_bez_znaku - skonwertowana_bez_znaku)
+        else:
+            return abs(oryginalna_bez_znaku - skonwertowana_bez_znaku) / abs(oryginalna_bez_znaku)
 
-# Przykład użycia
-posit_system = Posit(n=16, es=2)
-posit_value = 0b0111010110101011  # Przykładowa wartość Posit
-ieee_float = posit_system.posit_to_float(posit_value)
 
-# Przykładowa wartość rzeczywista odpowiadająca Posit (to powinno być obliczone lub znane)
+# Tutaj beda benchmarki dla kodu
+# zapis do plikow excelowskich
+formaty = [(32, 1), (32, 2), (32, 3), (32, 4)]
+ilosc_powtorzen = 50 
 
-print(f"Posit: {posit_value:016b}, IEEE 754 Float: {ieee_float}")
+for n, es in formaty:
+    plik = f"wyniki{n}_{es}.csv"
+    with open(plik, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Format', 'Liczba', 'Czas (ms)', 'IEEE 754 Float', 'Blad wzgledny'])
+
+        posit_system = Posit(n=n, es=es)
+        losowe_liczby = [format(random.randint(0, (1 << n) - 1), f'0{n}b') for _ in range(ilosc_powtorzen)]
+
+        for liczba in losowe_liczby:
+            pomiar_start = timeit.default_timer()
+            ieee_float = posit_system.posit_na_float(liczba)
+            pomiar_stop = timeit.default_timer()
+            czas_calkowity = (pomiar_stop - pomiar_start) * 1000  # ms
+
+            skonwertowany_posit = posit_system.float_na_posit(ieee_float)
+
+            blad_wzgledny = posit_system.oblicz_blad_wzgledny(int(liczba, 2), skonwertowany_posit)
+
+            writer.writerow([f"<{n},{es}>", liczba, f"{czas_calkowity:.6f}".replace('.', ','), ieee_float, f"{blad_wzgledny:.2f}".replace('.', ',')])
+
+    print(f"Zapisano wyniki do pliku CSV: {plik}")
+
